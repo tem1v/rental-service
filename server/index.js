97ab9e46ac8b728b734c480c2fca4b84ddbec0fe
Artyom Lyakhov <alyahov49@gmail.com>
2025-05-10 00:44:20 +0300

    Написал серверную часть, подключил PostgreSQL, добавил маршруты

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import * as dotenv from 'dotenv';
+import sequelize from "./config/database.js";
+import cors from 'cors';
+import router from "./routes/index.js";
+
+
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', router)
+
+const start = async () =>{
+	try {
+		await sequelize.authenticate();
+		await sequelize.sync();
+		app.listen(PORT, () => console.log(`Сервер запущен на порте ${PORT}`));
+	} catch (e) {
+		console.log(e);
+	}
+};
+
+start();
+
