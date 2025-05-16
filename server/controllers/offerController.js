d5fd6d9127706dfd43a2906e88f978e9c556f37f
Artyom Lyakhov <alyahov49@gmail.com>
2025-05-16 03:51:18 +0300

    Добавил предложения

--- server/controllers/offerController.js
+++ server/controllers/offerController.js
@@ -9,4 +9,86 @@ export async function getAllOffers(req, res, next){
 	}
 }
 
+export async function createOffer(req, res, next) {
+    try {
+        const {
+            title,
+            description,
+            publishDate,
+            city,
+            isPremium,
+            isFavorite,
+            rating,
+            type,
+            rooms,
+            guests,
+            price,
+            features,
+            commentsCount,
+            latitude,
+            longitude,
+            userId,
+        } = req.body;
+
+        if (!req.files?.previewImage || req.files.previewImage.length === 0) {
+            return next(
+                ApiError.badRequest(
+                    "Превью изображение обязательно для загрузки"
+                )
+            );
+        }
+
+        const previewImagePath = `/static/${req.files.previewImage[0].filename}`;
+
+        let processedPhotos = [];
+        if (req.files?.photos) {
+            processedPhotos = req.files.photos.map(
+                (file) => `/static/${file.filename}`
+            );
+        }
+
+        let parsedFeatures = [];
+        if (features) {
+            try {
+                parsedFeatures =
+                    typeof features === "string"
+                        ? JSON.parse(features)
+                        : features;
+            } catch {
+                parsedFeatures = features.split(",");
+            }
+        }
+
+        const offer = await Offer.create({
+            title,
+            description,
+            publishDate,
+            city,
+            previewImage: previewImagePath,
+            photos: processedPhotos,
+            isPremium,
+            isFavorite,
+            rating,
+            type,
+            rooms,
+            guests,
+            price,
+            features: parsedFeatures,
+            commentsCount,
+            latitude,
+            longitude,
+            authorId: userId,
+        });
+
+        return res.status(201).json(offer);
+    } catch (error) {
+        next(
+            ApiError.internal(
+                "Не удалось добавить предложение: " + error.message
+            )
+        );
+    }
+}
+   
+
 export default getAllOffers;
--- server/routes/offerRoutes.js
+++ server/routes/offerRoutes.js
@@ -1,9 +1,14 @@
 import {Router} from 'express';
-import { getAllOffers } from '../controllers/offerController.js';
+import { createOffer, getAllOffers } from "../controllers/offerController.js";
+import upload from '../middleware/upload.js';
 
 const router = new Router();
 
 router.get('/offers', getAllOffers);
+router.post('/offers', upload.fields([
+	{name: 'previewImage', maxCount: 1},
+	{name: 'photos', maxCount: 6}
+]), createOffer);
 
 
 export default router;
